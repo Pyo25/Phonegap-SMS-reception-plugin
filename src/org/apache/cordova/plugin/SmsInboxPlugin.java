b1f0ab2f3eb3717a5f186bb3f0d21109bf024032
Pyo25 <orban.pierreyves@gmail.com>
2013-04-02 19:04:29 +0200

    First commit

---
+++ src/org/apache/cordova/plugin/SmsInboxPlugin.java
@@ -0,0 +1,100 @@
+/*
+Copyright (C) 2013 by Pierre-Yves Orban
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+
+public class SmsInboxPlugin extends CordovaPlugin {
+	public final String ACTION_HAS_SMS_POSSIBILITY = "HasSMSPossibility";
+	public final String ACTION_RECEIVE_SMS = "StartReception";
+	public final String ACTION_STOP_RECEIVE_SMS = "StopReception";
+	
+	private CallbackContext callback_receive;
+	private SmsReceiver smsReceiver = null;
+	
+	public SmsInboxPlugin() {
+		super();
+	}
+	
+	@Override
+	public boolean execute(String action, JSONArray arg1,
+			final CallbackContext callbackContext) throws JSONException {
+		
+		if (action.equals(ACTION_HAS_SMS_POSSIBILITY)) {
+			
+			Activity ctx = this.cordova.getActivity();
+			if(ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)){
+				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
+			} else {
+				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, false));
+			}
+			return true;
+		}
+		else if (action.equals(ACTION_RECEIVE_SMS)) {
+			
+			if(this.smsReceiver == null) {
+				this.smsReceiver = new SmsReceiver();
+				IntentFilter fp = new IntentFilter();
+			    fp.addAction("android.provider.Telephony.SMS_RECEIVED");
+			    fp.setPriority(1000);
+			    this.cordova.getActivity().registerReceiver(this.smsReceiver, fp);
+			}
+			
+			this.smsReceiver.startReceiving(callbackContext);
+	
+			PluginResult pluginResult = new PluginResult(
+					PluginResult.Status.NO_RESULT);
+			pluginResult.setKeepCallback(true);
+			callbackContext.sendPluginResult(pluginResult);
+			this.callback_receive = callbackContext;
+			
+			return true;
+		}
+		else if(action.equals(ACTION_STOP_RECEIVE_SMS)) {
+			
+			smsReceiver.stopReceiving();
+			
+			// 1. Stop the receiving context
+			PluginResult pluginResult = new PluginResult(
+					PluginResult.Status.NO_RESULT);
+			pluginResult.setKeepCallback(false);
+			this.callback_receive.sendPluginResult(pluginResult);
+			
+			// 2. Send result for the current context
+			pluginResult = new PluginResult(
+					PluginResult.Status.OK);
+			callbackContext.sendPluginResult(pluginResult);
+			
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ src/org/apache/cordova/plugin/SmsReceiver.java
@@ -0,0 +1,87 @@
+/*
+Copyright (C) 2013 by Pierre-Yves Orban
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.PluginResult;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SmsReceiver extends BroadcastReceiver {
+	
+	public static final String SMS_EXTRA_NAME = "pdus";
+	private CallbackContext callback_receive;
+	private boolean isReceiving;
+	
+	// This broadcast boolean is used to continue or not the message broadcast
+	// to the other BroadcastReceivers waiting for an incoming SMS
+	private boolean broadcast;
+	
+	public SmsReceiver() {
+		super();
+		this.isReceiving = true;
+		this.broadcast = false;
+	}
+	
+	public void startReceiving(CallbackContext ctx) {
+		this.callback_receive = ctx;
+		this.isReceiving = true;
+	}
+
+	public void stopReceiving() {
+		this.callback_receive = null;
+		this.isReceiving = false;
+	}
+	
+	@Override
+	public void onReceive(Context ctx, Intent intent) {
+		
+		this.abortBroadcast();
+			
+		// Get the SMS map from Intent
+	    Bundle extras = intent.getExtras();
+	    if (extras != null)
+	    {
+		   // Get received SMS Array
+			Object[] smsExtra = (Object[]) extras.get( SMS_EXTRA_NAME );
+			for (int i=0; i < smsExtra.length; i++)
+			{
+				SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsExtra[i]);
+				if(this.isReceiving && this.callback_receive != null) {
+					String formattedMsg = sms.getOriginatingAddress() + ">" + sms.getMessageBody();
+		        	PluginResult result = new PluginResult(PluginResult.Status.OK, formattedMsg);
+		           	result.setKeepCallback(true);
+		            callback_receive.sendPluginResult(result);
+				}
+			}
+
+	        if(!this.isReceiving || this.broadcast) {
+	        	this.clearAbortBroadcast();
+	        }
+	     }
+	}
+}
